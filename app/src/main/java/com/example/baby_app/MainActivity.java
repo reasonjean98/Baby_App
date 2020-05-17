9d3e5643edbec551079afee6f51460bddc45b4fe
reasonjean98 <u.jin@kunsan.ac.kr>
2020-05-17 16:49:35 +0900

    체험학습 인텐트 설정

--- app/src/main/java/com/example/baby_app/MainActivity.java
+++ app/src/main/java/com/example/baby_app/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button library, park, toilet, museum_art, perform;
+    Button library, park, toilet, museum_art, perform, field_study;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         toilet = (Button) findViewById(R.id.toilet);
         museum_art = (Button) findViewById(R.id.museum_art);
         perform = (Button) findViewById(R.id.perform);
+        field_study = (Button) findViewById(R.id.field_study);
 
         library.setOnClickListener(
                 new Button.OnClickListener() {
@@ -66,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
 
                         Intent intent = new Intent(getApplicationContext(), toiletActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        field_study.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(getApplicationContext(), field_studyActivity.class);
                         startActivity(intent);
                     }
                 }
