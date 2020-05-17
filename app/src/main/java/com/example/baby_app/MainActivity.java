558cbfab736827ae8ab683f8ef20440ebac8ac1a
reasonjean98 <u.jin@kunsan.ac.kr>
2020-05-17 16:39:49 +0900

    각 페이지 인텐트 설정

--- app/src/main/java/com/example/baby_app/MainActivity.java
+++ app/src/main/java/com/example/baby_app/MainActivity.java
@@ -8,19 +8,64 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button library, park, toilet, museum_art, perform;
 
-    Button library;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button library = (Button) findViewById(R.id.library);
+        library = (Button) findViewById(R.id.library);
+        park = (Button) findViewById(R.id.park);
+        toilet = (Button) findViewById(R.id.toilet);
+        museum_art = (Button) findViewById(R.id.museum_art);
+        perform = (Button) findViewById(R.id.perform);
+
         library.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
 
                         Intent intent = new Intent(getApplicationContext(), libraryActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        park.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(getApplicationContext(), parkActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        perform.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(getApplicationContext(), performActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        museum_art.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(getApplicationContext(), museum_artActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        toilet.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(getApplicationContext(), toiletActivity.class);
                         startActivity(intent);
                     }
                 }
--- app/src/main/java/com/example/baby_app/libraryActivity.java
+++ app/src/main/java/com/example/baby_app/libraryActivity.java
@@ -1,6 +1,11 @@
 package com.example.baby_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +16,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class libraryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Spinner spinner;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
 
 
     @Override
@@ -21,9 +31,29 @@ public class libraryActivity extends AppCompatActivity implements OnMapReadyCall
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("200m");
+        arrayList.add("500m");
+        arrayList.add("1000m");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, arrayList);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 
     @Override
