f82dc6473d3b4cd20e99cfef2bcdd3342ce70178
reasonjean98 <u.jin@kunsan.ac.kr>
2020-05-19 19:45:01 +0900

    각 페이지 spinner 장착

--- app/src/main/java/com/example/baby_app/parkActivity.java
+++ app/src/main/java/com/example/baby_app/parkActivity.java
@@ -3,6 +3,10 @@ package com.example.baby_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class parkActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class parkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park);
+
+        Spinner s = (Spinner)findViewById(R.id.spinner_location1);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Toast.makeText(getApplicationContext(),parent.getItemAtPosition(position)+"가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
     }
 }
