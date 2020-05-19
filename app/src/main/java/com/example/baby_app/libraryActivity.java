6d1082f60f7423d4ba951ef758153fb6ab2fa430
reasonjean98 <u.jin@kunsan.ac.kr>
2020-05-19 23:28:21 +0900

    페이지별 spinner 수정

--- app/src/main/java/com/example/baby_app/libraryActivity.java
+++ app/src/main/java/com/example/baby_app/libraryActivity.java
@@ -21,10 +21,6 @@ import java.util.ArrayList;
 public class libraryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Spinner spinner;
-    ArrayList<String> arrayList;
-    ArrayAdapter<String> arrayAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,52 +30,21 @@ public class libraryActivity extends AppCompatActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        arrayList = new ArrayList<>();
-        arrayList.add("200m");
-        arrayList.add("500m");
-        arrayList.add("1000m");
-
-        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_spinner_dropdown_item, arrayList);
-
-        spinner = (Spinner)findViewById(R.id.spinner);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-                switch (i){
-                    case 0:
-                        Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
-                                Toast.LENGTH_SHORT).show();
-                    case 1:
-                        Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
-                                Toast.LENGTH_SHORT).show();
-                    case 2:
-                        Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
-                                Toast.LENGTH_SHORT).show();
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
     }
-
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
         mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        LatLng kunsan = new LatLng(35.9452863, 126.6799643);
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
+        markerOptions.position(kunsan);
         markerOptions.title("현재 위치");
         mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kunsan));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
     }
 
 }
---
+++ app/src/main/java/com/example/baby_app/study_DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.baby_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class study_DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study__detail);
+    }
+}
